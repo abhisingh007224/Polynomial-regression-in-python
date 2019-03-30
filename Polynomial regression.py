fe141bb850b9bfc331ebcb2bf27880de6f0e1d84
abhisingh007224 <42486179+abhisingh007224@users.noreply.github.com>
2019-03-30 18:24:47 +0530

    Add files via upload

---
+++ Polynomial regression.py
@@ -0,0 +1,47 @@
+Importing the Libraries
+      import pandas as pd
+
+      import numpy as np
+
+      import matplotlib.pyplot as plt
+
+Load the dataset
+       dataset=pd.read_csv('# your csv file ')
+
+       X= dataset.iloc[].values         # X is a independent variable
+
+       y= dataset.iloc[].values         # y is a dependent variable
+     
+Encoding categorical data into numerical if required
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
+labelencoder_X=LabelEncoder()
+      
+X[categorical column]=labelencoder_X.fit_transform(X[])
+      
+onehotencoder=OneHotEncoder(categorical_features =[])
+      
+X=onehotencoder.fit_transform(X).toarray()
+fitting poly_reg to the dataset
+  from sklearn.preprocessing import PolynomialFeatures
+
+  poly_reg=PolynomialFeatures(degree=4)
+
+  X_poly=poly_reg.fit_transform(X)
+
+  lin_reg2=LinearRegression()
+
+  lin_reg2.fit(X_poly,y)
+
+Visualising the result
+  plt.scatter(X,y,color='red')
+
+  plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='blue')
+
+  plt.title('')
+
+  plt.xlabel('')
+
+  plt.ylabel('')
+
+  plt.show()
